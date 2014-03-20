3f281f4ef101561f373c7f2e35b6a43e6a6d8ded
mel--mel <p_mel@windowslive.com>
2014-03-20 13:53:23 +0200

    functions split

--- src/histogrammata.c
+++ src/histogrammata.c
@@ -12,80 +12,106 @@
 #include <stdlib.h>
 #include "lodepng.h"
 
+void print_RGB_values(unsigned width, unsigned height, int input_channels, unsigned char *image){
+
+	int x, y, pos;
+
+	printf("RGB values are:\n");
+
+	//for (y=0; y < 2; y++) { // --> first 2 rows
+	for (y=0; y < height; y++) {
+		//for (x=0; x < 2; x++) { //--> first 2 columns
+		for (x=0; x < width; x++) {
+			printf("|");
+			for (pos=0; pos < 3; pos++) {
+			    printf("%d ", image[y*width*input_channels + x*input_channels + pos]);
+			    }
+			printf("|");
+			}
+	    printf("\n");
+		}
+}
+
+void make_grayscale_pixel_values_array(unsigned width, unsigned height, int input_channels, unsigned char *image, int pixel_values[width][height]){
+	int x, y;
+
+	for (y=0; y < height; y++) {
+			for (x=0; x < width; x++) {
+				pixel_values[x][y] = image[y*width*input_channels + x*input_channels];
+				}
+		    }
+}
+
+void print_pixel_values(unsigned width, unsigned height, int pixel_values[width][height]){
+	int x, y;
+
+	for (y=0; y < height; y++) {
+			for (x=0; x < width; x++) {
+				printf("|%d|  ", pixel_values[x][y]);
+				}
+			printf("\n");
+			}
+}
+
+void change_RGB_values(unsigned width, unsigned height, int input_channels, unsigned char *image){
+	int x, y, pos;
+
+	for (y=100; y < 150; y++) { // --> first 2 rows
+			//for (y=0; y < height; y++) {
+				for (x=100; x < 150; x++) { //--> first 2 columns
+				//for (x=0; x < width; x++) {
+				    for (pos=0; pos < 3; pos++) {
+				        image[y*width*input_channels + x*input_channels + pos] = 255;
+				        }
+				    }
+				 }
+}
+
 int main(void) {
 
-	unsigned char* image;
-	unsigned width, height, x, y, error;
-	int pos, input_channels;
+	unsigned char* image;//, *new_image;
+	unsigned width, height, error;
+	//int pos;
+	int input_channels;
 	int pixel_values[256][256];
 
+
+	/*open image + handle error*/
 	input_channels = 4;
 
 	error = lodepng_decode32_file(&image, &width, &height, "LenaDark.png");
 	if(error) {
-		printf("error %u: %s\n", error, lodepng_error_text(error));
-	}
+		printf("error %u: %s\n", error, lodepng_error_text(error));}
 	else{
-		printf("Image successfully open!\n");
-	}
+		printf("Image successfully open!\n");}
 
+	/*print width + height*/
 	printf("\nwidth = %u\n", width);
 	printf("height = %u\n\n\n", height);
-/*
-	//printf("RGB values are:\n");
 
-	for (y=0; y < 2; y++) { // --> first 2 rows
-	//for (y=0; y < height; y++) {
-	//	for (x=0; x < 2; x++) { //--> first 2 columns
-		for (x=0; x < width; x++) {
-		    printf("|");
-		    for (pos=0; pos < 3; pos++) {
-		        printf("%d ", image[y*width*input_channels + x*input_channels + pos]);
-		        }
-		    printf("|");
-		    }
-		 printf("\n");
-		 }
-*/
 
-	for (y=0; y < height; y++) {
-		for (x=0; x < width; x++) {
-			pixel_values[x][y] = image[y*width*input_channels + x*input_channels];
-			    }
-			 }
+	/*put pixel values in an array*/
+	make_grayscale_pixel_values_array(width, height, input_channels, image, pixel_values);
 
-	for (y=0; y < height; y++) {
-		for (x=0; x < width; x++) {
-			printf("|%d|  ", pixel_values[x][y]);
-			}
-		printf("\n");
-		}
+    /*print pixel values*/
+	//print_pixel_values(width, height, pixel_values);
 
-	/*for (y=100; y < 150; y++) { // --> first 2 rows
-		//for (y=0; y < height; y++) {
-			for (x=100; x < 150; x++) { //--> first 2 columns
-			//for (x=0; x < width; x++) {
-			    for (pos=0; pos < 3; pos++) {
-			        image[y*width*input_channels + x*input_channels + pos] = 255;
-			        }
-			    }
-			 }
+	/*count different pixel values*/
+	//TO DO
 
-	for (y=150; y < 180; y++) { // --> first 2 rows
-			//for (y=0; y < height; y++) {
-				for (x = 200; x < 250; x++) { //--> first 2 columns
-				//for (x=0; x < width; x++) {
-				    for (pos=1; pos < 3; pos++) {
-				        image[y*width*input_channels + x*input_channels + pos] = 0;
-				        }
-				    }
-				 } */
+	/*change image*/
+	change_RGB_values(width, height, input_channels, image);
 
-	/*Encode the image*/
-	 /* error = lodepng_encode32_file("test_img.png", image, width, height); */
+	/*print RGB values*/
+	//print_RGB_values(width, height, input_channels, image);
 
-	  /*if there's an error, display it*/
-	  if(error) printf("error %u: %s\n", error, lodepng_error_text(error));
+
+	 /*Encode the image + error handling*/
+	 error = lodepng_encode32_file("img_out.png", image, width, height);
+	 if(error){
+		 printf("error %u: %s\n", error, lodepng_error_text(error));}
+	 else{
+		 printf("Image successfully saved!\n");}
 
 	free(image);
 
