18d7f9c7ccada183a0bb2438a3996c39365e4171
mel--mel <p_mel@windowslive.com>
2014-03-22 19:23:00 +0200

    so happy - open image, 2d array allocation and filling, 2d array printing - correct

--- src/histogrammata.c
+++ src/histogrammata.c
@@ -32,17 +32,34 @@ void print_RGB_values(unsigned width, unsigned height, int input_channels, unsig
 		}
 }
 
+int **allocate_2d_array(unsigned width, unsigned height){
+	int i, *temp;
+	int **array_name;
+
+	array_name = (int **)malloc(width * sizeof(int*));
+	temp = (int *)malloc(width * height * sizeof(int));
+	if ((temp == NULL) || (array_name == NULL)) {
+		puts("malloc error!!");}
+	else {
+		puts("2d array successfully allocated!!");}
+	for (i = 0; i < width; i++){
+		array_name[i] = temp + (i * height);
+		}
+
+	return array_name;
+}
+
 void make_grayscale_pixel_values_array(unsigned width, unsigned height, int input_channels, unsigned char *image, int **pixel_values){
 	int x, y;
 
 	for (y=0; y < height; y++) {
 			for (x=0; x < width; x++) {
-				pixel_values[x][y] = image[y*width*input_channels + x*input_channels];
+				pixel_values[x][y] = (int)image[y*width*input_channels + x*input_channels];
 				}
 		    }
 }
 
-void print_pixel_values(unsigned width, unsigned height, int pixel_values[width][height]){
+void print_pixel_values(unsigned width, unsigned height, int **pixel_values){
 	int x, y;
 
 	for (y=0; y < height; y++) {
@@ -73,7 +90,7 @@ int main(void) {
 	unsigned width, height, error;
 	int i;//int pos;
 	int input_channels;
-	int **pixel_values, *temp;
+	int **pixel_values;
 
 
 	/*open image + handle error*/
@@ -89,13 +106,8 @@ int main(void) {
 	printf("\nwidth = %u\n", width);
 	printf("height = %u\n\n\n", height);
 
-
 	/*allocate a 2D array*/
-	pixel_values = (int **)malloc(width * sizeof(int*));
-	temp = malloc(width * height * sizeof(int));
-	for (i = 0; i < width; i++){
-		pixel_values[i] = temp + (i * height);
-	    }
+    pixel_values = allocate_2d_array(width, height);
 
 	/*put pixel values in the array*/
 	make_grayscale_pixel_values_array(width, height, input_channels, image, pixel_values);
